e89e32c6ec6319ab1a817d54be7c5761d70bb204
Gregory Vincic <g@7de.se>
2019-12-28 06:28:59 +0100

    Using nexus to print doctype html

--- doctype/tag.go
+++ doctype/tag.go
@@ -3,23 +3,31 @@ package doctype
 import (
 	"fmt"
 	"io"
+
+	"github.com/gregoryv/nexus"
 )
 
 type Tag struct {
 	children []interface{}
 	name     string
+	attr     []interface{}
 }
 
-func (t *Tag) WriteTo(w io.Writer) error {
-	fmt.Fprintf(w, "<%s>", t.name)
+func (t *Tag) WriteTo(w io.Writer) (int, error) {
+	p, err := nexus.NewPrinter(w)
+	t.OpenTo(p)
 	for _, child := range t.children {
 		switch child := child.(type) {
 		case *Tag:
-			child.WriteTo(w)
+			child.WriteTo(p)
 		case string:
-			fmt.Fprintf(w, child)
+			p.Print(child)
 		}
 	}
-	fmt.Fprintf(w, "</%s>", t.name)
-	return nil
+	p.Printf("</%s>", t.name)
+	return p.Written, *err
+}
+
+func (t *Tag) OpenTo(w io.Writer) (int, error) {
+	return fmt.Fprint(w, "<", t.name, ">")
 }
